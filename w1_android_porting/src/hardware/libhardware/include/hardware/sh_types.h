eeccb13973fc26716f763e9eb329ebcef481d0a4
Deven Fan <devenfan@gmail.com>
2012-02-03 21:59:52 +0800

    refactor
    Signed-off-by: Deven Fan <devenfan@gmail.com>

---
+++ w1_android_porting/src/hardware/libhardware/include/hardware/sh_types.h
@@ -0,0 +1,39 @@
+/*
+ *
+ * Copyright (c) 2011 Deven Fan <deven.fan@gmail.com>
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+#ifndef __SH_TYPES_H
+#define __SH_TYPES_H
+
+#include <asm/types.h>
+
+typedef __u8  BYTE;
+
+typedef __s8  BOOL;
+
+#ifndef TRUE
+#define TRUE   1
+#endif
+
+#ifndef FALSE
+#define FALSE  0
+#endif
+
+#endif // __SH_TYPES_H
