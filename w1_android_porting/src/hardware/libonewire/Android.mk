09524d7cc11e492bb68b230901c21a7f6c053449
Deven Fan <devenfan@gmail.com>
2012-02-19 10:44:41 +0800

    update Android.mk

--- w1_android_porting/src/hardware/libonewire/Android.mk
+++ w1_android_porting/src/hardware/libonewire/Android.mk
@@ -4,10 +4,31 @@
 
 # HAL module implemenation, not prelinked and stored in
 # hw/<ONEWIRE_HARDWARE_MODULE_ID>.<ro.product.board>.so
+
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_PRELINK_MODULE := false
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_SRC_FILES := w1_hal.c w1_hal_impl.cpp
+
+LOCAL_INCLUDES += $(LOCAL_PATH)
+
+LOCAL_SHARED_LIBRARIES := \
+	liblog 	\
+	libcutils
+
+LOCAL_SRC_FILES += \
+	sh_log.c		\
+	sh_thread.c		\
+	sh_util.c		\
+	w1_netlink_userspace.c	\
+	w1_netlink_util.c		\
+	w1_netlink_userservice.c	\
+	w1_hal_impl.cpp		\
+#	w1_hal.c
+
 LOCAL_MODULE := libonewire
+
 include $(BUILD_SHARED_LIBRARY)
