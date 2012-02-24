9be68a4f2cb2391edda963cebbd7d76e29e476c8
Deven Fan <devenfan@gmail.com>
2012-02-25 00:38:24 +0800

    update Android.mk
    Signed-off-by: Deven Fan <devenfan@gmail.com>

--- onewire_android_porting/frameworks/base/services/onewire_jni/Android.mk
+++ onewire_android_porting/frameworks/base/services/onewire_jni/Android.mk
@@ -1,13 +1,21 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+# [optional, user, eng]
+# eng = required
+# optinal = no install on target
+LOCAL_MODULE_TAGS := eng
+
+# All of the source files that we will compile.
 LOCAL_SRC_FILES:= \
 	com_android_server_onewire_OneWireNativeService.cpp \
     onload.cpp
 
+# Also need the JNI headers.
 LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE)
 
+# All of the shared libraries we link against.
 LOCAL_SHARED_LIBRARIES := \
     libandroid_runtime \
 	libcutils \
@@ -32,7 +40,17 @@ ifeq ($(WITH_MALLOC_LEAK_CHECK),true)
 	LOCAL_CFLAGS += -DMALLOC_LEAK_CHECK
 endif
 
+# No specia compiler flags.
+LOCAL_CFLAGS +=
+
 LOCAL_MODULE:= libandroid_onewire
+
+# Target install path.
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+
+# Don't prelink this library.  For more efficient code, you may want
+# to add this library to the prelink map and set this to true.
+LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
 
